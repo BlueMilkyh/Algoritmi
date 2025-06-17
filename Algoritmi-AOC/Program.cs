a529e98d5af2545425791e4b168e53c6a065e960
BlueMilkyh <aljaz.ski@gmail.com>
2025-06-17 23:07:42 +0200

    a

---
+++ Algoritmi-AOC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System;
+using System.Linq;
+using System.Reflection;
+namespace Algoritmi_AOC
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdravljeni v aoc :)");
+            Console.WriteLine("Izberite dan, za test");
+
+
+        }
+
+
+    }
+
+}
